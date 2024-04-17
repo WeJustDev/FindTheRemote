6aa27ba2500aef57fd0599c99ace6424e04ee2a1
eliooooooo <eliott.burkle@gmail.com>
2024-04-17 10:17:52 +0200

    feat: add functionnal toggle timer

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,10 +1,21 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public Toggle timerToggle;
+
+    void Start()
+    {
+        timerToggle.isOn = PlayerPrefs.GetInt("ShowTimer", 1) == 1;
+    }
     public void PlayGame()
     {
+        // Stocke la valeur actuelle du timer avant de charger la scène
+        PlayerPrefs.SetInt("ShowTimer", timerToggle.isOn ? 1 : 0);
+        PlayerPrefs.Save();
+        
         SceneManager.LoadScene("bedroom");
     }
     
