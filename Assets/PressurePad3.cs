d486d0371e5b10e1e9498a18919a4508280fd820
deveney <jeremy.deveney@etu.unistra.fr>
2024-04-19 17:58:27 +0200

    Ajout des papillons, finalisation de la scene

--- Assets/PressurePad3.cs
+++ Assets/PressurePad3.cs
@@ -3,12 +3,23 @@ using UnityEngine;
 public class PressurePad3 : MonoBehaviour
 {
     public bool hasBlueKey = false; // Indicateur pour savoir si la BlueKeyCrystal est présente
+    private GameObject particlesObject; // Référence à l'objet des particules
+
+    private void Start()
+    {
+        // Obtenez une référence à l'objet des particules
+        particlesObject = transform.Find("Particles").gameObject;
+        // Désactivez-le initialement
+        particlesObject.SetActive(false);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("BlueKeyCrystal"))
         {
             hasBlueKey = true;
+            // Activez l'objet des particules
+            particlesObject.SetActive(true);
         }
     }
 
@@ -17,6 +28,8 @@ public class PressurePad3 : MonoBehaviour
         if (other.CompareTag("BlueKeyCrystal"))
         {
             hasBlueKey = false;
+            // Désactivez l'objet des particules
+            particlesObject.SetActive(false);
         }
     }
 }
